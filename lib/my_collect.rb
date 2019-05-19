d2c40f7ab914931c14567ac625ed2d4cef1791ef
Reina Ewing <reina.castro3@gmail.com>
2019-05-19 02:54:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  new_array = []
+    i = 0 
+    while i < array.size  
+      block_given?
+      new_array << yield(array[i])
+      i += 1 
+    end 
+    new_array 
+end 
 
